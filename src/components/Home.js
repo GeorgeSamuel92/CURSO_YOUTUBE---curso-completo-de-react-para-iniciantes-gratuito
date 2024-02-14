8c68667aa6a6833c8d38e91326e921962f6bfab4
Jorge <george.samuel.ti2023@gmail.com>
2024-02-14 17:39:34 -0300

    aula 11

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,31 @@
-import React, { useState } from 'react';
+import { useState } from "react";
+import ProductsList from "./ProductsList";
 
 export default function Home() {
-    const [products, setProducts] = useState([
-        {
-            name: "Samsung S24 Ultra",
-            description: "producto teste adicionado",
-            worker: "Uanela",
-            id: 1,
-        },
-        {
-            name: "iPhone 15 Pro Max",
-            description: "producto teste adicionado",
-            worker: "Jobs",
-            id: 2,
-        },
-        {
-            name: "Xiaomi 13 Ultra",
-            description: "producto teste adicionado",
-            worker: "Buffet",
-            id: 3,
-        },
-    ]);
+  const [products, setProducts] = useState([
+    {
+      name: "Samsung S24 Ultra",
+      description: "producto teste adicionado",
+      worker: "Uanela",
+      id: 1,
+    },
+    {
+      name: "iPhone 15 Pro Max",
+      description: "producto teste adicionado",
+      worker: "Jobs",
+      id: 2,
+    },
+    {
+      name: "Xiaomi 13 Ultra",
+      description: "producto teste adicionado",
+      worker: "Buffet",
+      id: 3,
+    },
+  ]);
 
-    return (
-        <div className='home'>
-            <h2>Homepage</h2>
-            {products.map(products =>
-                <div className='product-preview' key={products.id}>
-                    <h2>{products.name}</h2>
-                    <p>Adcionado por {products.worker}</p>
-                </div>
-            )}
-
-
-        </div>
-    )
+  return (
+    <div className="home">
+      <ProductsList products={products} title="Todos Produtos" />
+    </div>
+  );
 }
---
+++ src/components/ProductsList.js
@@ -0,0 +1,14 @@
+
+export default function ProductsList(products, title) {
+  return (
+    <div className="products-list">
+        <h2>{title}</h2>
+      {products.map((product) => (
+        <div className="product-preview" key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Adicionado por {product.worker}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
