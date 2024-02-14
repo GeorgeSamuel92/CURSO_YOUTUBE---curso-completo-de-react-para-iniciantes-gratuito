50b731d8e481b2616f75245dc4ebf8d64847f512
Jorge <george.samuel.ti2023@gmail.com>
2024-02-14 15:24:27 -0300

    envio

---
+++ src/components/Navibar.js
@@ -0,0 +1,10 @@
+export default function Navibar() {
+    return <nav>
+        <h1> SideGeStock</h1>
+        <div className="link">
+            <a href="/">Home</a>
+            <a href="/create">Novo Produto</a>
+        </div>
+    </nav>
+
+}
