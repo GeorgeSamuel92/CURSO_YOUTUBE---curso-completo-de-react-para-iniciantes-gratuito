72336e952dbadabcd75ff622628dace16308a187
Jorge <george.samuel.ti2023@gmail.com>
2024-02-22 17:09:11 -0300

    aula_14

--- src/components/ProductsLists.js
+++ src/components/ProductsLists.js
@@ -1,1 +1,16 @@
-rfc
+import React from "react";
+
+export default function ProductsList({ products, title, handleDelete }) {
+  return (
+    <div className="products-list">
+      <h2>{title}</h2>
+      {products.map((product) => (
+        <div className="product-preview" key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Adicionado por {product.worker}</p>
+          <button onClick={() => handleDelete(product.id)}  style={{ color: '#000' }}>Apagar</button>
+        </div>
+      ))}
+    </div>
+  );
+}
