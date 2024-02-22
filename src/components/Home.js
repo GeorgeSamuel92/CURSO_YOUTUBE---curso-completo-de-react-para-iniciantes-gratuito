236bed15c6894794989ce2ebc6cf64262b5adc5b
Jorge <george.samuel.ti2023@gmail.com>
2024-02-22 17:39:05 -0300

    baseado na aula 15

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,49 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from "react";
+import ProductsList from "./ProductsLists.js";
 
 export default function Home() {
-    const [products, setProducts] = useState([
-        {
-            name: "Samsung S24 Ultra",
-            description: "producto teste adicionado",
-            worker: "Uanela",
-            id: 1,
-        },
-        {
-            name: "iPhone 15 Pro Max",
-            description: "producto teste adicionado",
-            worker: "Jobs",
-            id: 2,
-        },
-        {
-            name: "Xiaomi 13 Ultra",
-            description: "producto teste adicionado",
-            worker: "Buffet",
-            id: 3,
-        },
-    ]);
+  const [products, setProducts] = useState([
+    {
+      name: "Samsung S24 Ultra",
+      description: "producto teste adicionado",
+      worker: "Uanela",
+      id: 1,
+    },
+    {
+      name: "iPhone 15 Pro Max",
+      description: "producto teste adicionado",
+      worker: "Jobs",
+      id: 2,
+    },
+    {
+      name: "Xiaomi 13 Ultra",
+      description: "producto teste adicionado",
+      worker: "Uanela",
+      id: 3,
+    },
+  ]);
 
-    return (
-        <div className='home'>
-            <h2>Homepage</h2>
-            {products.map(products =>
-                <div className='product-preview' key={products.id}>
-                    <h2>{products.name}</h2>
-                    <p>Adcionado por {products.worker}</p>
-                </div>
-            )}
+  const [name, setName] = useState("Uanela");
 
+  const handleDelete = (id) => {
+    const newProdutcts = products.filter((product) => product.id !== id);
+    setProducts(newProdutcts);
+  };
 
-        </div>
-    )
+  useEffect(() => {
+    console.log("useEffect executou");
+    console.log(name);
+  }, [name, products] );
+
+  return (
+    <div className="home">
+      <ProductsList
+        products={products}
+        title="Todos Productos"
+        handleDelete={handleDelete}
+      />
+      <button onClick={() => setName('Como')} style={{color: 'black'}}>Trocar nome</button>
+      <p>{name}</p>
+    </div>
+  );
 }
