b1af6eb8a2478f44acc5c4d63cfe616f0feaf87b
George Samuel <george.samuel.ti2023@gmail.com>
2024-02-23 16:35:30 -0300

    aula_18

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,24 @@ import ProductsList from "./ProductsLists.js";
 export default function Home() {
     const [products, setProducts] = useState(null);
 
+    const [isLoading, setIsloading] = useState(true);
+
     useEffect(() => {
-        fetch('http://localhost:8000/products')
+        setTimeout(() => {
+            fetch('http://localhost:8000/products')
             .then((res) => res.json())
             .then((data) => {
                 setProducts(data);
-                console.log(data);
-    })
-}, []);
+                setIsloading(false);
+            })
+        }, 2000)
+    }, []);
 
 
-return (
-    <div className="home">
-        {products && <ProductsList products={products} title="Todos Productos" />}
-    </div>
-);
+    return (
+        <div className="home">
+            {isLoading && <p>Carregando...</p>}
+            {products && <ProductsList products={products} title="Todos Productos" />}
+        </div>
+    );
 }
