82062940e7a02bb58cae17a7caf88c1710c6e33e
George Samuel <george.samuel.ti2023@gmail.com>
2024-02-24 13:58:55 -0300

    aula_19

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,31 @@
 import { useState, useEffect } from "react";
 import ProductsList from "./ProductsLists.js";
 
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Home() {
     const [products, setProducts] = useState(null);
-
     const [isLoading, setIsloading] = useState(true);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/products')
-            .then((res) => res.json())
-            .then((data) => {
-                setProducts(data);
-                setIsloading(false);
-            })
-        }, 2000)
+                .then((res) => res.json())
+                .then((data) => {
+                    setProducts(data);
+                    setIsloading(false);
+                });
+        }, 1000);
     }, []);
-
 
     return (
         <div className="home">
-            {isLoading && <p>Carregando...</p>}
+            {isLoading && (
+                <div className="notch-container">
+                    <FontAwesomeIcon icon={faCircleNotch} className="loading-notch" />
+                </div>
+            )}
             {products && <ProductsList products={products} title="Todos Productos" />}
         </div>
     );
