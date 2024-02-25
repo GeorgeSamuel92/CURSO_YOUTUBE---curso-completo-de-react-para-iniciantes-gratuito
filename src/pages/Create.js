e558819631a1e0e17ae92826c85176d9845a1e99
George Samuel <george.samuel.ti2023@gmail.com>
2024-02-25 00:45:29 -0300

    aula_28 - Como fazer um Post Resquest no React

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,54 +1,66 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Create() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState();
   const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [worker, setWorker] = useState('Uanela');
+  const [worker, setWorker] = useState("Uanela");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e) => {
-    e.preventDefaut();
-    const product = {name, description, price, worker};
-    console.log(product);
+    e.preventDefault();
+    const product = { name, price, description, worker };
+
+    setIsLoading(true);
+    setTimeout(() => {
+      fetch("http://localhost:8000/products", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(product),
+      }).then(() => setIsLoading(false));
+    }, 2000);
   };
 
   return (
     <div className="create">
       <h2>Adicionar um Novo Producto</h2>
       <form onSubmit={handleSubmit}>
-        <label > Nome do Produto: </label>
+        <label>Nome do producto:</label>
         <input
           type="text"
+          required
           value={name}
           onChange={(e) => setName(e.target.value)}
-          required
         />
-
-        <label > Descrição: </label>
+        <label>Descrição:</label>
         <textarea
-          type="text"
+          required
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          required 
-          ></textarea>
-
-        <label > Preço: </label>
+        ></textarea>
+        <label>Preço:</label>
         <input
           type="number"
+          required
           value={price}
           onChange={(e) => setPrice(e.target.value)}
-          required />
-
-        <label > Nome do Funcionario; </label>
-        <select
-          value={worker}
-          onChange={(e) => setWorker(e.target.value)}
-        >
+        />
+        <label>Funcionário:</label>
+        <select value={worker} onChange={(e) => setWorker(e.target.value)}>
           <option value="Uanela">Uanela</option>
           <option value="Como">Como</option>
           <option value="Parker">Parker</option>
+          <option value="ArmisTrong">ArmisTrong</option>
         </select>
-        <button>Adicionar</button>
-
+        <button disabled={isLoading}>
+          {isLoading ? (
+            <FontAwesomeIcon icon={faCircleNotch} className="loading-notch" />
+          ) : (
+            "Adicionar Producto"
+          )}
+        </button>
       </form>
     </div>
   );
