54efd2b0638d48d35f1dbbb993971a78964568ba
George Samuel <george.samuel.ti2023@gmail.com>
2024-02-25 01:22:44 -0300

    aula_30 -  Como Fazer Delete Request no React

--- src/pages/ProductDetalis.js
+++ src/pages/ProductDetalis.js
@@ -1,15 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useFetch } from '../utils/useFetch';
 import ProductsDetailsSkeleton from '../components/ProductsDetailsSkeleton';
 import ErrorMessage from '../components/ErrorMessage';
+
 
 export default function ProductDetails() {
     const { id } = useParams();
     const { data: product, isError, isLoading, error } = useFetch("http://localhost:8000/products/" + id);
 
+    const navigate = useNavigate();
+    const handleDelete = () => {
+        fetch('http://localhost:8000/products/' + id, {
+            method: 'DELETE',
+        }).then(() => navigate('/'));
+    };
+
     return (
         <div className="product-details">
-            {isError && <ErrorMessage  message={error.message} />}
+            {isError && <ErrorMessage message={error.message} />}
             {isLoading && <ProductsDetailsSkeleton />}
             {product && (
                 <article>
@@ -17,6 +25,7 @@ export default function ProductDetails() {
                     <p>Preço: {product.price}</p>
                     <p>Adcionado por: {product.worker}</p>
                     <div className='description' > {product.description}</div>
+                    <button onClick={handleDelete} >Apagar Produto</button>
                 </article>
             )}
         </div>
